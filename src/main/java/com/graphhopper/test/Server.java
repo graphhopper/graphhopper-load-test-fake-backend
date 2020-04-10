a4438a6cb7a0698733868b0affdd8f1f77efeaac
Peter <graphhopper@gmx.de>
2020-04-10 18:21:53 +0200

    allow different paths (direct or via lb)

--- src/main/java/com/graphhopper/test/Server.java
+++ src/main/java/com/graphhopper/test/Server.java
@@ -1,9 +1,11 @@
 package com.graphhopper.test;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -38,20 +40,26 @@ public class Server extends AbstractVerticle {
                     putHeader("content-type", "text/html").
                     end("<html><body><h2>Fake Server</h2></body></html>");
         });
-        router.route("/optimize*").handler(routingContext -> {
+        Handler<RoutingContext> lambda1 = routingContext -> {
             // to avoid parsing the JSON put the jobId in the header
             String jobId = UUID.randomUUID().toString();
             routingContext.response().
                     putHeader("content-type", "application/json").
                     putHeader("X-GH-JobId", jobId).
                     end("{\"job_id\":\"" + jobId + "\"}");
-        });
+        };
+        // handle request as "back end"
+        router.route("/optimize*").handler(lambda1);
+        // handle requests directly
+        router.route("/api/1/vrp/optimize*").handler(lambda1);
 
-        router.route("/solution*").handler(routingContext -> {
+        Handler<RoutingContext> lambda2 = routingContext -> {
             routingContext.response().
                     putHeader("content-type", "application/json").
                     end(jsonStr);
-        });
+        };
+        router.route("/solution*").handler(lambda2);
+        router.route("/api/1/vrp/solution*").handler(lambda2);
 
         int port = config().getInteger("http.port", 8080);
         vertx.createHttpServer().requestHandler(router).listen(port);
